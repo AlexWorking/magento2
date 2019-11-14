3e44f5da63e6d4e1449864be70fff3d644945e1b
AlexWorking <workmail2500@gmail.com>
2019-11-15 01:42:24 +0200

    First Commit

---
+++ app/code/Potoky/EyelensProduct/Model/Product/Type/Eyelens.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Potoky\EyelensProduct\Model\Product\Type;
+
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+class Eyelens extends \Magento\GroupedProduct\Model\Product\Type\Grouped
+{
+    const TYPE_CODE = 'eyelens';
+}
---
+++ app/code/Potoky/EyelensProduct/Model/Product/Type/Eyelens/Price.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Potoky\EyelensProduct\Model\Product\Type\Eyelens;
+
+
+class Price extends \Magento\Catalog\Model\Product\Type\Price
+{
+
+}
